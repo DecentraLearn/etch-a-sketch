8232f2efd268b30c1ca61930f4111054ee57f86f
Garrett Franklin <garrettfranklin@pm.me>
2022-04-02 22:23:14 -0600

    Finish the grid layout
    Dynamically render tiles to fit the display, regardless of grid size.

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 // Create a reference to the display container that will hold all the tiles
 const display = document.querySelector(".display");
 
+// Define the grid size
+let gridSize = 20;
+
 // Create a function that creates an individual tile element with class="tile"
 function createTile() {
   const tile = document.createElement("div");
   tile.setAttribute("class", "tile");
+  tile.style.width = `${100 / gridSize}%`;
   return tile;
 }
 
-// Define the grid size
-let gridSize = 16;
+// Create a function to build a row
+function createRow() {
+  const row = document.createElement("div");
+  row.setAttribute("class", "row");
+  row.style.height = `${100 / gridSize}%`;
+  return row;
+}
 
 // Add a tile child element to the display for each grid spot
-const grid = [];
-for (let i = 0; i < (gridSize * gridSize); i++) {
-  const tile = createTile();
-  grid.push(tile);
+for (let i = 0; i < gridSize; i++) {
+  const row = createRow();
+  for (let k = 0; k < gridSize; k++) {
+    const tile = createTile();
+    row.appendChild(tile);
+  }
+  display.appendChild(row)
 }
-grid.forEach(x => display.appendChild(x));
 
