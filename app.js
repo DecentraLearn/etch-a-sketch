923003bb8306751c17d9556b45aa0aa14666fc62
Garrett Franklin <garrettfranklin@pm.me>
2022-04-03 07:36:08 -0600

    Build a reset button
    Add a reset button that clears the grid and rebuilds a fresh one.
    Currently no way to change the grid size.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const display = document.querySelector(".display");
 
 // Define the grid size
-let gridSize = 20;
+let gridSize = 16;
 
 // Create a function that creates an individual tile element with class="tile"
 function createTile() {
@@ -20,20 +20,35 @@ function createRow() {
   return row;
 }
 
-// Add a tile child element to the display for each grid spot
-for (let i = 0; i < gridSize; i++) {
-  const row = createRow();
-  for (let k = 0; k < gridSize; k++) {
-    const tile = createTile();
-    row.appendChild(tile);
+// Add a function that adds a tile child element to the display for each grid spot
+function createGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    const row = createRow();
+    for (let k = 0; k < gridSize; k++) {
+      const tile = createTile();
+      row.appendChild(tile);
+    }
+    display.appendChild(row)
   }
-  display.appendChild(row)
+  // Add the "active" class to any tile when hovered over with mouse
+  function setActive(e) {
+    e.target.classList.add("active");
+  }
+  const tiles = document.querySelectorAll(".tile");
+  tiles.forEach(t => t.addEventListener("mouseover", setActive));
 }
 
-// Add the "active" class to any tile when hovered over with mouse
-function setActive(e) {
-  e.target.classList.add("active");
-}
-const tiles = document.querySelectorAll(".tile");
-tiles.forEach(t => t.addEventListener("mouseover", setActive));
+// Render a grid on page load
+createGrid();
 
+// Define the onclick function for a button that removes the current grid, and builds a clean one using current gridSize
+function resetGrid() {
+  // Remove current tiles from display
+  const rows = document.querySelectorAll(".row");
+  rows.forEach(row => row.remove());
+  createGrid();
+}
+
+// Link resetGrid function to the reset button
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetGrid);
